0cab2712ff00b3d614b03adfd72321ccf6fd80f5
Rownok <53168870+rownokmahbub@users.noreply.github.com>
2023-10-30 18:48:43 +0600

    gitignore

--- app/admin/admission/[id]/page.js
+++
@@ -1,7 +0,0 @@
-export async function generateStaticParams({params}) {
-    const posts = await fetch('http://localhost:4000/v1/student/pending-students/id').then((res) => res.json())
-   
-    return posts.map((post) => ({
-      id: post.id,
-    }))
-  }
