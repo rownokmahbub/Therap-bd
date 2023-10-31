be1c783dd3307d2bc4c981283e1ea3ae5a249723
Rownok <53168870+rownokmahbub@users.noreply.github.com>
2023-10-31 10:07:31 +0600

    update favicon

--- app/layout.js
+++ app/layout.js
@@ -10,6 +10,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className="light">
+
       <body className="">
         <main className=''>
           <Providers>
