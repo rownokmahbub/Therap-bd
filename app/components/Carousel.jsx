a235c840195896cb1651e9003f4ea93586091285
Rownok <53168870+rownokmahbub@users.noreply.github.com>
2023-11-02 18:17:57 +0600

    hero section updated

--- app/components/Carousel.jsx
+++ app/components/Carousel.jsx
@@ -32,6 +32,25 @@ export default class Carousel extends Component {
 
   <div className="flex flex-col lg:flex-row justify-center lg:justify-between lg:gap-10 items-center lg:h-[80vh]">
   <div className='max-w-4xl w-full '>
+  <p className=' text-sm lg:text-lg  bg-yellow-100 dark:bg-yellow-800 px-4 py-1 my-5 w-fit rounded-xl border-l-8 border-yellow-300 text-yellow-600 dark:text-yellow-200'>With Therap BD</p>
+  <h1 className='text-base lg:text-5xl capitalize font-bold mt-5'> Therap BD Employee with Raska Urzoshi Maa'm</h1>
+  <p className='text-xs lg:text-md text-slate-400 dark:text-slate-300 font-medium max-w-lg my-5'>Through a combination of lectures, readings, and discussions, students
+will gain a solid foundation in educational psychology.</p>
+ <Button className='shadow-xl ' color='primary' size='md' radius='sm'>
+  All Courses
+ </Button>
+  </div>
+  <Image className='max-w-xl mt-4 w-full xl:h-[500px] object-cover rounded-3xl' loading='lazy' object-fit='contain' height={600} width={1000}  src="/therap.png" alt="image" />
+  </div>
+ 
+</div>
+          </div>
+        <div className="w-full px-2 md:px-0">
+          <div className=" relative container lg:mx-auto light-bg dark:dark-bg py-2 mt-5 lg:mt-10 rounded-xl border-4 border-slate-100 dark:border-slate-500 dark:shadow-2xl px-3 lg:px-10 ">
+
+
+  <div className="flex flex-col lg:flex-row justify-center lg:justify-between lg:gap-10 items-center lg:h-[80vh]">
+  <div className='max-w-4xl w-full '>
   <p className=' text-sm lg:text-lg  bg-yellow-100 dark:bg-yellow-800 px-4 py-1 my-5 w-fit rounded-xl border-l-8 border-yellow-300 text-yellow-600 dark:text-yellow-200'>Top Leading Company</p>
   <h1 className='text-base lg:text-5xl capitalize font-bold mt-5'>Join the leading
 software company</h1>
